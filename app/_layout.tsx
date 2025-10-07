8e5724a61236e21045b1b328ee507a79ff477f3a
JeanBeq <jean.bequilleux@gmail.com>
2025-10-07 11:44:59 +0200

    feat(redux): integrate Redux Provider in app layout

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,16 +1,23 @@
 import { Stack } from 'expo-router';
+import { Provider } from 'react-redux';
+import { PersistGate } from 'redux-persist/integration/react';
 import { usePersistCurrentUrl } from '../hooks/use-persisted-url';
+import { persistor, store } from './store';
 
 export default function RootLayout() {
   usePersistCurrentUrl();
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    >
-      <Stack.Screen name="index" />
-      <Stack.Screen name="(main)" />
-    </Stack>
+    <Provider store={store}>
+      <PersistGate loading={null} persistor={persistor}>
+        <Stack
+          screenOptions={{
+            headerShown: false,
+          }}
+        >
+          <Stack.Screen name="index" />
+          <Stack.Screen name="(main)" />
+        </Stack>
+      </PersistGate>
+    </Provider>
   );
 }
